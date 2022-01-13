bad578726e05a48f74493ea7869455b2745c3ec3
Claudio Maldonado <claum0395@gmail.com>
2022-01-12 22:30:47 -0500

    Deploy v0.0.2

--- src/app/public/components/image/image.component.ts
+++ src/app/public/components/image/image.component.ts
@@ -1,7 +1,6 @@
 
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { Subscription } from 'rxjs';
 import { EventService } from 'src/app/services/event.service';
