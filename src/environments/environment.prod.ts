be9bec783e1318d8bb20c702be875e053c6d244d
Claudio Mldo <clam0395@gmail.com>
2023-03-13 11:30:00 -0500

    Arreglo en tarjeta de imagen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  serverWs: 'ws://34.224.255.163/ws',
-  baseUrl: 'http://34.224.255.163/api',
+  serverWs: 'wss://34.224.255.163/ws',
+  baseUrl: 'https://34.224.255.163/api',
   cryptoSecretKey: "p:jG4.@B.]/rFw{RVp-mU6&8e=e%2U<)",
-  repoUrl: 'http://23.22.88.68/api/v1'
+  repoUrl: 'https://23.22.88.68/api/v1'
 };
