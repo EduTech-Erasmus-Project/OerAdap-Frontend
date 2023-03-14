0af15fdd4a9d3731c44f74497a6b2d915c4d4f78
Claudio Mldo <clam0395@gmail.com>
2023-03-14 11:17:33 -0500

    Cambio de dominios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  serverWs: 'wss://34.224.255.163/ws',
-  baseUrl: 'https://34.224.255.163/api',
+  serverWs: 'wss://oeradap.ups.edu.ec/ws',
+  baseUrl: 'https://oeradap.ups.edu.ec/api',
   cryptoSecretKey: "p:jG4.@B.]/rFw{RVp-mU6&8e=e%2U<)",
   repoUrl: 'https://23.22.88.68/api/v1'
 };
